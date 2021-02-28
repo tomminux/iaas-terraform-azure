f6f0290f313b2eefb6dfb4991362bd477ae78dd4
Paolo Arcagni <tomminux@users.noreply.github.com>
2021-02-28 07:56:14 +0100

    Added DNS management to vnet

--- exec-tf.sh
+++ exec-tf.sh
@@ -5,11 +5,17 @@
 
 usage(){
 	echo "
-  Usage: $0 <infrastructure_module> [<pip|nopip(default)>]
+  Usage: $0 <infrastructure_module> [<pip|nopip(default)> | <dns|nodns>]
  
   infrastructure_module: rg | vnet | peering | generic-server
-  pip:                   allocates Public IP(s)
-  nopip:                 does not allocate Public IP(s)
+
+  when using generic-server
+  	pip:                   allocates Public IP(s)
+  	nopip:                 does not allocate Public IP(s)
+
+  when using vnet
+	dns:				   user an internal DNS SERVER (vnet configuration)
+	nodns: 				   user Generic Azure DNS for this VNET
 "
 	exit 1
 }
@@ -17,11 +23,11 @@ usage(){
 ## ..:: main ::..
 ## ----------------------------------------------------------------------------
 
-IS_MODULE_NAME_SET=0
+IS_LINUX_MODULE_NAME_SET=0
 
 [[ $# -eq 0 ]] && usage
 
-if [[ $1 == "rg" || $1 == "vnet" || $1 == "peering" ]]
+if [[ $1 == "rg" || $1 == "peering" ]]
 then
         echo "Setting working dir to $1"
 	WORKING_DIR=$1
@@ -34,11 +40,29 @@ then
 	case "$2" in
 		"pip" )
 		__MODULE_NAME__="linux-server"
-		IS_MODULE_NAME_SET=1
+		IS_LINUX_MODULE_NAME_SET=1
 		;;
 		"nopip" )
 		__MODULE_NAME__="linux-server-no-public-ip"
-		IS_MODULE_NAME_SET=1
+		IS_LINUX_MODULE_NAME_SET=1
+		;;
+	        *)
+		usage
+	esac
+
+elif [[ $1 == "vnet" ]]
+then
+
+	echo "Setting working dir to $1"
+	WORKING_DIR=$1
+	case "$2" in
+		"dns" )
+		__LINE_TO_ADD__="  dns_servers         = var.vnet_configurations[count.index].dns_servers"
+		IS_DNS=1
+		;;
+		"nodns" )
+		__LINE_TO_ADD__=""
+		IS_DNS=1
 		;;
 	        *)
 		usage
@@ -52,11 +76,18 @@ fi
 
 
 cd $WORKING_DIR
-if (( $IS_MODULE_NAME_SET ))
+if (( $IS_LINUX_MODULE_NAME_SET ))
 then
 	echo "Replacing __MODULE_NAME__ with $__MODULE_NAME__"
 	cp main.template main.tf
         sed -i "s/__MODULE_NAME__/$__MODULE_NAME__/g" main.tf
+fi
+
+if (( $IS_DNS ))
+then
+	echo "adding line $__LINE_TO_ADD__ to vnet/modules/networking/main.tf"
+	cp modules/networking/main.template modules/networking/main.tf
+    sed -i "s/__LINE_TO_ADD__/$__LINE_TO_ADD__/g" modules/networking/main.tf
 fi
 
 terraform init
@@ -64,11 +95,5 @@ terraform plan
 
 read -p "Do you want to actaully appy this? (any key to continue, ctlr+c to exit here)" -n 1 -r
 echo    # (optional) move to a new line
-#if [[ $REPLY =~ ^[Yy]$ ]]
-#then
-    terraform apply
-#else
-#    echo "Got it, you just wanted to test it, fair enough! Ciao"
-#    exit 0
-#fi
 
+terraform apply
